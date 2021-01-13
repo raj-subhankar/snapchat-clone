cdacc13c6dc573d158e605bc997a2a31ace0e09e
Subhankar Raj <subhankar.rj@gmail.com>
2021-01-13 12:55:05 +0530

    Implement WebcamCapture

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import { Router, Switch } from 'react-router-dom';
 import './App.css';
+import Preview from './Preview';
+import WebcamCapture from './WebcamCapture';
 
 function App() {
   return (
     <div className="app">
-      <h1>Snapchat</h1>
+
+      <Router>
+        <div className='app__body'>
+          <Switch>
+          <Route exact path='/preview'>
+              <Preview />
+            </Route>
+
+            <Route exact path='/'>
+              <WebcamCapture />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+
+      <WebcamCapture />
     </div>
   );
 }
---
+++ src/WebcamCapture.js
@@ -0,0 +1,48 @@
+import React, { useCallback, useRef, useState } from 'react'
+import './WebcamCapture.css'
+import Webcam from 'react-webcam'
+import RadioButtonUncheckedIcon from '@material-ui/icons/RadioButtonUnchecked'
+import { useDispatch } from 'react-redux'
+import { setCameraImage } from './features/cameraSlice'
+import { useHistory } from 'react-router-dom'
+
+const videoConstraints = {
+    width: 250,
+    height: 400,
+    facingMode: 'user'
+}
+
+function WebcamCapture() {
+    const webcamRef = useRef(null)
+    const dispatch = useDispatch()
+    const history = useHistory()
+
+    const [image, setImage] = useState(null)
+
+    const capture = useCallback(() => {
+        const imageSrc = webcamRef.current.getScreenshot()
+        dispatch(setCameraImage(imageSrc))
+        history.push('/preview')
+
+    }, [webcamRef])
+
+    return(
+        <div className='webcamCapture'>
+            <Webcam 
+                audio={false}
+                height={videoConstraints.height}
+                width={videoConstraints.width}
+                ref={webcamRef}
+                screenshotFormat='image/jpeg'
+                videoConstraints={videoConstraints}
+            />
+
+            <RadioButtonUncheckedIcon 
+                className='webcamCapture__button' 
+                onClick={capture}
+                fontSize='large'/>
+        </div>
+    )
+}
+
+export default WebcamCapture
